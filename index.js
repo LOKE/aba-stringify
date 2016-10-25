3754301dafefa725251ab7c429e7ac9c361ae959
Dominic Smith <dom@ip-172-16-0-216.ap-southeast-2.compute.internal>
2016-10-25 16:22:41 +1100

    Init commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const TRANSACTION_CODES = ['13', '50', '51', '52', '53', '54', '55', '56', '57'];
+const INDICATOR = ['N', 'W', 'X', 'Y', ' '];
+const bsbRegex = /\d{3}\-\d{3}/;
+
+function pad(char, right, input, n) {
+  let str = input == null ? '' : String(input);
+
+  if (str.length > n) {
+    throw new Error(`Value "${str}" is longer than ${n}`);
+  }
+
+  while (str.length < n) {
+    str = right ? str + char : char + str;
+  }
+
+  return str;
+}
+
+const leftPad = pad.bind(null, ' ', false);
+const rightPad = pad.bind(null, ' ', true);
+const zeroPad = pad.bind(null, '0', false);
+
+function formatDate(date) {
+  return [
+    zeroPad(date.getDate(), 2),
+    zeroPad(date.getMonth() + 1, 2),
+    zeroPad(date.getYear() % 100, 2)
+  ].join('');
+}
+
+function formatHeader(header) {
+  if (header.fiAbbreviation.length !== 3) throw new Error('Invalid header abbreviation');
+
+  return [
+    '0                 ',
+    zeroPad(header.reelSequenceNumber, 2),
+    header.fiAbbreviation,
+    '       ',
+    rightPad(header.preferredSpecification, 26),
+    zeroPad(header.userIdentification, 6),
+    rightPad(header.description, 12),
+    formatDate(header.date),
+    '                                        \n'
+  ].join('');
+}
+
+function formatRow(row) {
+  const indicator = row.indicator || ' ';
+
+  if (TRANSACTION_CODES.indexOf(row.transactionCode) === -1) throw new Error(`Invalid transactionCode "${row.transactionCode}"`);
+  if (INDICATOR.indexOf(indicator) === -1) throw new Error(`Invalid indicator "${indicator}"`);
+  if (!bsbRegex.test(row.bsb)) throw new Error(`Invalid bsb "${row.bsb}"`);
+  if (!bsbRegex.test(row.traceRecordBsb)) throw new Error(`Invalid traceRecordBsb "${row.traceRecordBsb}"`);
+
+  return [
+    '1',
+    row.bsb,
+    leftPad(row.accountNumber, 9),
+    indicator,
+    row.transactionCode,
+    zeroPad(row.amount, 10),
+    rightPad(row.accountTitle, 32),
+    rightPad(row.lodgementReference, 18),
+    row.traceRecordBsb,
+    leftPad(row.traceRecordAccountNumber, 9),
+    rightPad(row.remitterName, 16),
+    zeroPad(row.withhealdTax, 8),
+    '\n'
+  ].join('');
+}
+
+function formatFooter(rows) {
+  const creditTotal = rows.reduce((t, r) => t + (r.transactionCode >= 50 ? r.amount : 0), 0);
+  const debitTotal = rows.reduce((t, r) => t + (r.transactionCode < 50 ? r.amount : 0), 0);
+  const netTotal = Math.abs(creditTotal - debitTotal);
+
+  return [
+    '7999-999            ',
+    zeroPad(netTotal, 10),
+    zeroPad(creditTotal, 10),
+    zeroPad(debitTotal, 10),
+    '                        ',
+    zeroPad(rows.length, 6),
+    '                                        \n'
+  ].join('');
+}
+
+function abaStringify(header, rows) {
+  return rows.reduce((out, r) => out + formatRow(r), formatHeader(header)) + formatFooter(rows);
+}
+
+module.exports = abaStringify;
---
+++ test.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+const test = require('ava');
+const abaStringify = require('./');
+
+function macro(t, header, rows, expected) {
+  const actual = abaStringify(header, rows);
+  t.is(actual, expected);
+}
+
+test(macro, {
+  reelSequenceNumber: 1,
+  fiAbbreviation: 'BQL',
+  preferredSpecification: 'MY NAME',
+  userIdentification: '111111',
+  description: '1004231633',
+  date: new Date('2010-04-23')
+}, [
+  {
+    bsb: '123-456',
+    accountNumber: '157108231',
+    indicator: ' ',
+    transactionCode: '53',
+    amount: 1234,
+    accountTitle: 'S R SMITH',
+    lodgementReference: 'TEST BATCH',
+    traceRecordBsb: '062-000',
+    traceRecordAccountNumber: '12223123',
+    remitterName: 'MY ACCOUNT',
+    withhealdTax: 1200
+  },
+  {
+    bsb: '123-783',
+    accountNumber: '12312312',
+    indicator: ' ',
+    transactionCode: '53',
+    amount: 2200,
+    accountTitle: 'J K MATTHEWS',
+    lodgementReference: 'TEST BATCH',
+    traceRecordBsb: '062-000',
+    traceRecordAccountNumber: '12223123',
+    remitterName: 'MY ACCOUNT',
+    withhealdTax: 30
+  },
+  {
+    bsb: '456-789',
+    accountNumber: '125123',
+    indicator: ' ',
+    transactionCode: '53',
+    amount: 3123513,
+    accountTitle: 'P R JONES',
+    lodgementReference: 'TEST BATCH',
+    traceRecordBsb: '062-000',
+    traceRecordAccountNumber: '12223123',
+    remitterName: 'MY ACCOUNT',
+    withhealdTax: 0
+  },
+  {
+    bsb: '121-232',
+    accountNumber: '11422',
+    indicator: ' ',
+    transactionCode: '53',
+    amount: 2300,
+    accountTitle: 'S MASLIN',
+    lodgementReference: 'TEST BATCH',
+    traceRecordBsb: '062-000',
+    traceRecordAccountNumber: '12223123',
+    remitterName: 'MY ACCOUNT',
+    withhealdTax: 0
+  }
+], fs.readFileSync(path.resolve(__dirname, 'fixtures/first.aba'), 'utf8'));
